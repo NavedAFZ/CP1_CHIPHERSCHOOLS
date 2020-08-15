bb74403293669a01879d1025d7e4935e571aa86c
NavedAFZ <39985811+NavedAFZ@users.noreply.github.com>
2020-08-15 14:36:33 +0530

    Add files via upload

---
+++ pyramid.cpp
@@ -0,0 +1,28 @@
+# include<bits/stdc++.h>
+using namespace std;
+/*int main()
+{
+    int rows;
+    cin>>rows;
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}*/
+int main()
+{
+    int rows;
+    cin>>rows;
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<rows-i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
